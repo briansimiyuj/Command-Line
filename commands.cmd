50eb51f013543566621f388317032c5135624eed
Brian Simiyu <briansimiyuj@gmail.com>
2022-09-18 15:35:44 +0300

    changes to the root directory

--- commands.cmd
+++ commands.cmd
@@ -10,3 +10,4 @@ ls -l = Shows long listings of the contents in the current directory
 
 cd ${directory} = changes directory
 cd .. = changes to the previous directory
+cd / = changes to the root directory
