bc91c65d4c2463541cbeb30a411861824fc8e8d8
Brian Simiyu <briansimiyuj@gmail.com>
2022-09-18 15:21:10 +0300

    Shows long listings of the contents in the current

--- commands.cmd
+++ commands.cmd
@@ -4,4 +4,5 @@ whoami = current user
 pwd = current working directory
 ls = lists contents of the current directory
 ls ${directory} =  lists contents of the ${directory}
-ls -a = Shows hidden files of the current directory 
+ls -a = Shows hidden files of the current directory
+ls -l = Shows long listings of the contents in the current directory
